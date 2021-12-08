dc93e13e764ab4be6e62b00bf6afe58ca370b331
Akshay Kamble <kamblea635@gmail.com>
2021-12-08 21:16:49 +0530

    added remove function and seprated play function

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-window.addEventListener('keydown',function(e){
+window.addEventListener('keydown',play)
+function play(e){
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
 
     if(!audio) return;
     audio.currentTime = 0; // to rewind it to start
     audio.play()
-    console.log(key)
-})
+    key.classList.add('playing')
+    }
+    
+function remove(e){
+if(e.propertyName !== 'transform') return
+this.classList.remove('playing')
+}
+const keys = document.querySelectorAll('.key')
+
+keys.forEach(key=> key.addEventListener("transitionend",remove))
